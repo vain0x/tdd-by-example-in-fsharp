08f704e8740b719305db15abe003991d03da64c8
vain0x <vainzerox@gmail.com>
2018-08-02 06:48:06 +0900

    Group tests with a module

--- MulticurrencyTests/Tests.fs
+++ MulticurrencyTests/Tests.fs
@@ -7,13 +7,14 @@ open Multicurrency
 let is<'T> (actual: 'T) (expected: 'T) =
   Assert.Equal(expected, actual)
 
-[<Fact>]
-let testMultiplication () =
-  let five = Dollar(5)
-  five.Times(2) |> is (Dollar(10))
-  five.Times(3) |> is (Dollar(15))
+module DollarTests =
+  [<Fact>]
+  let testMultiplication () =
+    let five = Dollar(5)
+    five.Times(2) |> is (Dollar(10))
+    five.Times(3) |> is (Dollar(15))
 
-[<Fact>]
-let testEquality () =
-  Dollar(5) |> is (Dollar(5))
-  Dollar(5) <> Dollar(6) |> is true
+  [<Fact>]
+  let testEquality () =
+    Dollar(5) |> is (Dollar(5))
+    Dollar(5) <> Dollar(6) |> is true
