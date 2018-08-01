92384fa8a613b1b43e4d753aa2786b2b9c45ea89
vain0x <vainzerox@gmail.com>
2018-08-02 06:20:34 +0900

    Remove unnecessary mutable field

--- Multicurrency/Library.fs
+++ Multicurrency/Library.fs
@@ -1,9 +1,6 @@
 namespace Multicurrency
 
 type Dollar(amount: int) =
-  // Redeclare amount as mutable field.
-  let mutable amount = amount
-
   member this.Times(multiplier) =
     Dollar(amount * multiplier)
 
